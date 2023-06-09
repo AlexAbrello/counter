2662e4912a99047f6c74edf9a74437f2cf2d5370
AlexAbrello <abrello063@gmail.com>
2023-06-09 12:38:51 +0300

    SeparateCounter with redux

--- src/components/separateCounter/SeparateCounter.tsx
+++ src/components/separateCounter/SeparateCounter.tsx
@@ -1,14 +1,23 @@
 import React, {useEffect, useState} from 'react';
 import {Settings} from "../settings/Settings";
 import {Counter} from "../counter/Counter";
+import {useDispatch, useSelector} from "react-redux";
+import {AppRootStateType} from "../../state/store";
+import {
+  CounterType,
+  incrementCountAC,
+  resetCountAC,
+  setOnMaxValueAC,
+  setOnStartValueAC, setStartCountAC
+} from "../../reducers/countReducer";
 
 export const SeparateCounter = () => {
 
-  const [startValue, setStartValue] = useState<number>(0)
-  const [maxValue, setMaxValue] = useState<number>(1)
-
-  const [count, setCount] = useState<number>(0)
-  const [disabled, setDisabled] = useState<boolean>(true)
+  // const [startValue, setStartValue] = useState<number>(0)
+  // const [maxValue, setMaxValue] = useState<number>(1)
+  //
+  // const [count, setCount] = useState<number>(0)
+  // const [disabled, setDisabled] = useState<boolean>(true)
 
   // useEffect(() => {
   //   let startValue = localStorage.getItem('startValue')
@@ -31,24 +40,36 @@ export const SeparateCounter = () => {
   //   localStorage.setItem('maxValue', JSON.stringify(maxValue))
   // }, [maxValue])
 
+  const state = useSelector<AppRootStateType, CounterType>(state => state.counter)
+  const dispatch = useDispatch()
+
+  const startValue = state.startValue
+  const maxValue = state.maxValue
+  const count = state.count
+  const disabled = state.disabled
 
   const setOnStartValueChange = (value: number) => {
-    setStartValue(value)
-    setDisabled(true)
+    // setStartValue(value)
+    // setDisabled(true)
+    dispatch(setOnStartValueAC(value))
   }
   const setOnMaxValueChange = (value: number) => {
-    setMaxValue(value)
-    setDisabled(true)
+    // setMaxValue(value)
+    // setDisabled(true)
+    dispatch(setOnMaxValueAC(value))
   }
   const incrementCount = () => {
-    setCount(prev => prev + 1)
+    // setCount(prev => prev + 1)
+    dispatch(incrementCountAC())
   }
   const resetCount = () => {
-    setCount(startValue)
+    // setCount(startValue)
+    dispatch(resetCountAC())
   }
   const setStartCount = () => {
-    setCount(startValue)
-    setDisabled(false)
+    // setCount(startValue)
+    // setDisabled(false)
+    dispatch(setStartCountAC())
   }
 
   return (
