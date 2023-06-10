21e7defeeac866a686205c9b72f4bf74c1fcdd1e
AlexAbrello <abrello063@gmail.com>
2023-06-10 16:34:59 +0300

    SeparateCounter with redux (optimized) part 2

--- src/components/separateCounter/SeparateCounter.tsx
+++ src/components/separateCounter/SeparateCounter.tsx
@@ -4,26 +4,6 @@ import {Counter} from "../counter/Counter";
 
 
 export const SeparateCounter = memo(() => {
-  // useEffect(() => {
-  //   let startValue = localStorage.getItem('startValue')
-  //   let maxValue = localStorage.getItem('maxValue')
-  //   if (startValue) {
-  //     let currentStartValue = JSON.parse(startValue)
-  //     setStartValue(currentStartValue)
-  //   }
-  //   if (maxValue) {
-  //     let currentMaxValue = JSON.parse(maxValue)
-  //     setMaxValue(currentMaxValue)
-  //   }
-  // }, [])
-  //
-  // useEffect(() => {
-  //   localStorage.setItem('startValue', JSON.stringify(startValue))
-  // }, [startValue])
-  //
-  // useEffect(() => {
-  //   localStorage.setItem('maxValue', JSON.stringify(maxValue))
-  // }, [maxValue])
 
   return (
       <div className={'App'}>
